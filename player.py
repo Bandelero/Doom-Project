19ab9d530333d3ddfa6ba54c1d19aaa9dd9e9d38
Andrew Martinez <apetermz@gmail.com>
2024-01-18 19:33:22 -0800

    Added other textures to walls and mouse control

--- player.py
+++ player.py
@@ -44,16 +44,25 @@ class Player:
     def check_wall(self, x, y):
         return (x, y) not in self.game.map.world_map
     def check_wall_collision(self, dx, dy):
-        if self.check_wall(int(self.x + dx), int(self.y)):
+        scale = PLAYER_SIZE_SCALE / self.game.delta_time
+        if self.check_wall(int(self.x + dx * scale), int(self.y)):
             self.x += dx
-        if self.check_wall(int(self.x), int(self.y + dy)):
+        if self.check_wall(int(self.x), int(self.y + dy * scale)):
             self.y += dy
 
     def draw(self):
-        # pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
-        #              (self.x * 100 + WIDTH * math.cos(self.angle),
-        #               self.y * 100 + WIDTH * math.sin(self.angle)), 2)
+        pg.draw.line(self.game.screen, 'yellow', (self.x * 100, self.y * 100),
+                     (self.x * 100 + WIDTH * math.cos(self.angle),
+                      self.y * 100 + WIDTH * math.sin(self.angle)), 2)
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
+
+    def mouse_control(self):
+        mx, my = pg.mouse.get_pos()
+        if mx < MOUSE_BORDER_LEFT or mx > MOUSE_BORDER_RIGHT:
+            pg.mouse.set_pos([HALF_WIDTH, HALF_HEIGHT])
+        self.rel = pg.mouse.get_rel()[0]
+        #left off here. video at 23:03
+
     def update(self):
         self.movement()
 
